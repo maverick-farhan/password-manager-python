1dc53d043b2ffb8d24123cfda9298f5bc84b14e4
maverick-farhan <mdfarhanofficial@gmail.com>
2023-12-28 22:47:06 +0530

    changes made

--- pw.py
+++ pw.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 # pw.py - An insecure password locker program
 import randompswd
+import sys
 import pyperclip
+PASSWORD = {
+        'gmail':'iO;2C-dx@UTXm;XP>bvu',
+        'apple':'mB_5$2\'Ku0ez,":!J<^o',
+        'linux':'fJ<7i!~OwyRTmg+//oeo'
+        }
+if len(sys.argv)<2:
+    print('Usage: py pw.py [accout] - copy account password')
+    sys.exit()
+account = sys.argv[1]
 print('Press y/Y to create random password. Press n/N to cancel')
 generate = input()
 if generate == 'y' or generate =='Y' :
@@ -9,6 +19,9 @@ if generate == 'y' or generate =='Y' :
     passwd_created = randompswd.passwd_gen()
     pyperclip.copy(passwd_created )
 
-PASSWORD = {
-        'email':''
-        }
+if account in PASSWORD:
+    pyperclip.copy(PASSWORD[account])
+    print('Your Password for '+ account + 'copied to clipboard')
+else:
+    print('There is no account '+ account)
+
